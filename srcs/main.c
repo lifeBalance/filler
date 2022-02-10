d396a9068feab246cb4292c46e5ee827e4e065c7
Javier <javi@grey.local>
2022-02-10 23:06:10 +0200

    refactor main.c: move first board parsing operation to the main while loop (out from 'init_filler')

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 16:22:48 by rodrodri          #+#    #+#             */
-/*   Updated: 2022/02/09 18:21:01 by rodrodri         ###   ########.fr       */
+/*   Updated: 2022/02/10 23:04:59 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,20 @@ int	main(void)
 
 	if (init_filler(&f) < 0)
 		return (-1);
-	print_char2darr(f.board); // <=== delete me!!!!!
 	while (get_next_line(STDIN_FILENO, &f.line) > 0)
 	{
-		if (ft_strstr(f.line, PIECE_SIZE_LN))
-			handle_piece(&f);
-		else if (ft_strstr(f.line, LAST_MOVE_LN))
-		{
-			if (check_play(&f) < 0)
-				break ;
-		}
-		else if (ft_strstr(f.line, FIRST_BOARD_LN))
+		if (ft_strstr(f.line, FIRST_BOARD_LN))
 		{
 			parse_board(&f);
+			// make_filler_heatmap(&f);
 			print_char2darr(f.board); // <=== delete me!!!!!
 		}
+		else if (ft_strstr(f.line, PIECE_SIZE_LN))
+			handle_piece(&f);
+		else if (ft_strstr(f.line, LAST_MOVE_LN) && check_play(&f) < 0)
+			break ;
+		else
+			ft_strdel(&f.line);
 	}
 	free_char_2darr(f.board);
 	system("leaks rodrodri.filler");
@@ -51,12 +50,8 @@ static int	init_filler(t_filler *f)
 		return (-1);
 	if (get_size(&f->line, &f->b_rows, &f->b_cols) < 0)
 		return (-1);
-	if (find_line(&f->line, FIRST_BOARD_LN) <= 0)
-		return (-1);
 	f->board = alloc_char_2darr(f->b_rows, f->b_cols);
 	if (!f->board)
-		return (-1);
-	if (parse_board(f) < 0)
 		return (-1);
 	f->next_turn = PLAYA1;
 	return (0);
